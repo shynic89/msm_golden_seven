1f22bb036a66e51464f489c29ea9700aec00db5f
Nick Su <hsu0@chicagobooth.edu>
2017-07-25 19:24:42 +0000

    director test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,16 +3,58 @@ class MoviesController < ApplicationController
     @alld = Director.all
     render("/movies/index.html.erb")
   end
-  def read_row
-     render("/movies/read_row.html.erb")
-  end
+  
   def delete_row
-     render("/movies/delete_row.html.erb")
+     @id = params[:some_id]
+     @todelete = Director.find(@id)
+     @todelete.destroy
+     
+     redirect_to("/")
   end 
   def new_form
      render("/movies/new_form.html.erb")
   end 
+  def create_form
+    @new = Director.new
+    @new.name = params["director_name"]
+    @new.bio = params["director_bio"]
+    @new.dob = params["director_dob"]
+    @new.image_url = params["director_url"]
+    @new.save
+    
+     render("/movies/create_form.html.erb")
+  end 
   def edit_form
+     @id = params[:number]
+     @d = Director.find(@id)
+     @name = @d.name
+     @bio = @d.bio
+     @dob = @d.dob
+     @image = @d.image_url
+     
      render("/movies/edit_form.html.erb")
+  end
+  
+  def update_form
+    @id = params[:an_id]
+    @direct = Director.find(@id)
+    @direct.name = params["director_name"]
+    @direct.dob = params["director_dob"]
+    @direct.bio = params["director_bio"]
+    @direct.image_url = params["director_url"]
+    @direct.save
+    
+    redirect_to("/directors/"+@id)
+  end
+  
+  def read_row
+    @id = params[:the_id]
+    @d = Director.find(@id)
+    @name = @d.name
+    @bio = @d.bio
+    @dob = @d.dob
+    @image = @d.image_url
+    
+    render("read_row")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
+    
+  get("/", {:controller => "movies", :action => "index"})
+  get("/directors", {:controller => "movies", :action => "index"})
+
+  get("/delete_director/:some_id", {:controller => "movies", :action =>"delete_row"})
+  get("/directors/new_form", {:controller => "movies", :action => "new_form"})
+  get("/directors/create_form", {:controller => "movies", :action => "create_form"})
+  get("/directors/:number/edit_form", {:controller => "movies", :action => "edit_form"})
+  get("/update_director/:an_id", {:controller =>"movies", :action=> "update_form"})
+  
+
+  get("/directors/:the_id", {:controller =>"movies", :action => "read_row"})
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
-  
-  get("/", {:controller => "movies", :action => "index"})
-  get("/directors", {:controller => "movies", :action => "index"})
-  get("/directors/:number", {:controller =>"movies", :action => "read_row"})
-  get("/delete_director/:number", {:controller => "movies", :action =>"delete_row"})
-  get("/directors/new_form", {:controller => "movies", :action => "new_form"})
-  get("/directors/:number/edit_form", {:controller => "movies", :action => "edit_form"})
+
 end
