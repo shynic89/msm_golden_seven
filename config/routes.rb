5949f1ed9e2932fcd502217f12b82b16983af83d
Nick Su <hsu0@chicagobooth.edu>
2017-07-25 03:14:03 +0000

    director test 1

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
+  
+  get("/", {:controller => "movies", :action => "index"})
+  get("/directors", {:controller => "movies", :action => "index"})
+  get("/directors/:number", {:controller =>"movies", :action => "read_row"})
+  get("/delete_director/:number", {:controller => "movies", :action =>"delete_row"})
+  get("/directors/new_form", {:controller => "movies", :action => "new_form"})
+  get("/directors/:number/edit_form", {:controller => "movies", :action => "edit_form"})
 end
